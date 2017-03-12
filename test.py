9ef0f69e0870218ee54d11fc63ecd9d2be3001db
Chance Johnson <chanceworks@yahoo.com>
2017-03-12 16:25:48 -0500

    Created a test file

---
+++ test.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+
+
+class TestGenerate_maze_depth_first_search(TestCase):
+    def test_generate_maze_depth_first_search(self):
+        self.fail()
+
+
+class TestGet_open_tile_coordinates(TestCase):
+    def test_get_open_tile_coordinates(self):
+        self.fail()
+
+
+class TestDraw_maze(TestCase):
+    def test_draw_maze(self):
+        self.fail()
+
+
+class TestGet_cell_rect(TestCase):
+    def test_get_cell_rect(self):
+        self.fail()
+
+
+class TestDraw_door_object(TestCase):
+    def test_draw_door_object(self):
+        self.fail()
+
+
+class TestDraw_chest_object(TestCase):
+    def test_draw_chest_object(self):
+        self.fail()
+
+
+class TestDraw_key_object(TestCase):
+    def test_draw_key_object(self):
+        self.fail()
+
+
+class TestDraw_player_object(TestCase):
+    def test_draw_player_object(self):
+        self.fail()
